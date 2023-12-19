e5eb0be4023465d7b0a55eaf52bfaf01d4e75743
Marwan <marwan00mr@gmail.com>
2023-12-19 19:48:03 +0200

    ..

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { createServer } = require('http');
+const { createServer } = require('https');
 //import http from 'node:http';
 const { Server } = require('socket.io');
 //const { setupWebSocket } = require('./socket.cjs');
